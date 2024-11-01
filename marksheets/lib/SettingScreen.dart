64b56db7e0c1be3c3366e2495c5d553b922f1d2c
C:\Users\kyoze <tepenhasuitai@gamil.com>
2024-11-02 05:49:38 +0900

    マークの種類選択

--- marksheets/lib/SettingScreen.dart
+++ marksheets/lib/SettingScreen.dart
@@ -18,6 +18,9 @@ class _SettingScreen extends State<SettingScreen>{
   int _timeLimitHour = 1; // 選択された時間
   int _timeLimitMinute = 0; //分
   int _timeLimitSecond = 0; //秒
+  String selectedMarkType = 'A-Z'; // デフォルトは 'A-Z'
+  final List<String> markTypes = ['a-z', 'A-Z', '0-9', '+-', 'あ-ん', 'ア-ン'];
+  List<String> selectedMarkTypes = [];
 
   @override
   void dispose() {
@@ -106,7 +109,9 @@ class _SettingScreen extends State<SettingScreen>{
                     )
                 ],
               ),
-          SizedBox(height: 20),
+            SizedBox(height: 20),
+          
+          buildMarkTypeSelector(selectedMarkTypes),      
 
           //設定完了！！！！！！
           ElevatedButton(
@@ -166,4 +171,97 @@ class _SettingScreen extends State<SettingScreen>{
                     ],
                   );
   }
+  // マークタイプ選択のUI
+Widget buildMarkTypeSelector(List<String> selectedMarkTypes) {
+  // 選択可能なマークタイプ一覧
+  List<String> markTypes_PlusMinus = ['+', '-', '±'];
+  List<String> markTypes_Number = List.generate(10, (index) => String.fromCharCode('0'.codeUnitAt(0) + index));
+  List<String> markTypes_A2Z = List.generate(26, (index) => String.fromCharCode('A'.codeUnitAt(0) + index));
+  List<String> markTypes_a2z = List.generate(26, (index) => String.fromCharCode('a'.codeUnitAt(0) + index));
+  List<String> markTypes_Hiragana = [
+  'あ', 'い', 'う', 'え', 'お', 
+  'か', 'き', 'く', 'け', 'こ',
+  'さ', 'し', 'す', 'せ', 'そ',
+  'た', 'ち', 'つ', 'て', 'と',
+  'な', 'に', 'ぬ', 'ね', 'の',
+  'は', 'ひ', 'ふ', 'へ', 'ほ',
+  'ま', 'み', 'む', 'め', 'も',
+  'や',       'ゆ',       'よ',
+  'ら', 'り', 'る', 'れ', 'ろ',
+  'わ',             'を', 'ん'
+];
+List<String> markTypes_Katakana = [
+  'ア', 'イ', 'ウ', 'エ', 'オ', 
+  'カ', 'キ', 'ク', 'ケ', 'コ',
+  'サ', 'シ', 'ス', 'セ', 'ソ',
+  'タ', 'チ', 'ツ', 'テ', 'ト',
+  'ナ', 'ニ', 'ヌ', 'ネ', 'ノ',
+  'ハ', 'ヒ', 'フ', 'ヘ', 'ホ',
+  'マ', 'ミ', 'ム', 'メ', 'モ',
+  'ヤ',       'ユ',       'ヨ',
+  'ラ', 'リ', 'ル', 'レ', 'ロ',
+  'ワ',             'ヲ', 'ン'
+];
+
+  List<String> markTypes = [
+  ...markTypes_PlusMinus,
+  ...markTypes_Number,
+  ...markTypes_A2Z,
+  ...markTypes_a2z,
+  ...markTypes_Hiragana,
+  ...markTypes_Katakana,
+];
+
+    return Wrap(
+      spacing: 8.0,
+      children: List.generate(markTypes.length, (index) {
+        return GestureDetector(
+          onTap: () {
+            print('Tapped: ${markTypes[index]}');
+            setState(() {
+              // タップ時にリストへ追加・削除
+              if (selectedMarkTypes.contains(markTypes[index])) {
+                selectedMarkTypes.remove(markTypes[index]);
+              } else {
+                selectedMarkTypes.add(markTypes[index]);
+              }
+              selectedMarkTypes.sort((a, b) {
+              int groupA = _getMarkTypeGroup(a);
+              int groupB = _getMarkTypeGroup(b);
+              if (groupA == groupB) {
+                return a.compareTo(b); // 同じグループ内での通常のソート
+              }
+              return groupA.compareTo(groupB); // グループ間でのソート
+              });
+            });    
+            print(selectedMarkTypes);
+          },
+          child: CircleAvatar(
+            backgroundColor: selectedMarkTypes.contains(markTypes[index])
+                ? Colors.blue
+                : Colors.grey,
+            radius: 10,  
+            child: Text(markTypes[index], style: TextStyle(color: Colors.white, fontSize: 12)),            
+          ),
+        );
+      }),
+    );
+  }
+}
+
+int _getMarkTypeGroup(String mark) {
+  if (mark == '+' || mark == '-' || mark == '±') {
+    return 0; // 記号
+  } else if (mark.codeUnitAt(0) >= '0'.codeUnitAt(0) && mark.codeUnitAt(0) <= '9'.codeUnitAt(0)) {
+    return 1; // 数字
+  } else if (mark.codeUnitAt(0) >= 'A'.codeUnitAt(0) && mark.codeUnitAt(0) <= 'Z'.codeUnitAt(0)) {
+    return 2; // 大文字アルファベット
+  } else if (mark.codeUnitAt(0) >= 'a'.codeUnitAt(0) && mark.codeUnitAt(0) <= 'z'.codeUnitAt(0)) {
+    return 3; // 小文字アルファベット
+  } else if (mark.codeUnitAt(0) >= 'あ'.codeUnitAt(0) && mark.codeUnitAt(0) <= 'ん'.codeUnitAt(0)) {
+    return 4; // ひらがな
+  } else if (mark.codeUnitAt(0) >= 'ア'.codeUnitAt(0) && mark.codeUnitAt(0) <= 'ン'.codeUnitAt(0)) {
+    return 5; // カタカナ
+  }
+  return 6; // その他
 }
